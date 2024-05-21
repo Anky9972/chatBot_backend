4f98065084f38ed9cf2e995844181d356920f2bf
Vivek Gaur <ankygaur9972@gmail.com>
2024-05-21 11:19:07 +0530

    update

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const app =express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-app.use(express.json())
-app.use(cors())
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
-const PORT = 4000 || 8000;
+app.use(express.json());
+app.use(cors({
+  origin: 'https://portfolio-vivek-gaur-02.vercel.app' 
+}));
 
-const botroute = require('./bot')
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 4000;
+
+const botroute = require('./bot');
 app.use('/api/v1', botroute);
 
 // Home route
 app.get('/', (req, res) => {
-    res.send('This is HOME page');
+  res.send('This is HOME page');
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`App started successfully at ${PORT}`);
+  console.log(`App started successfully at ${PORT}`);
 });
-
